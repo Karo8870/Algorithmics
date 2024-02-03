e6f7c355c0c0dab7fe514c45f82afb918933ad2f
Karo8870 <cazacuchristian@gmail.com>
2024-02-03 23:56:52 +0200

    #1489 Bile1

--- main.cpp
+++ main.cpp
@@ -1,61 +1,43 @@
 #include<fstream>
-using namespace std;
-
-ifstream cin("necuatie.in");
-ofstream cout("necuatie.out");
 
 using namespace std;
 
-#define MOD 555557
+ifstream cin("bile1.in");
+ofstream cout("bile1.out");
 
-long long modPow(long long x, long long e) {
-    long long r = 1;
-    for (; e; e >>= 1) {
-        if (e & 1)
-            r = (r * x) % MOD;
-        x = (x * x) % MOD;
+typedef int big[300];
+
+void bigDouble(big x) {
+    int i, t = 0;
+    for (i = 1; i <= x[0]; i++, t /= 10) {
+        t += x[i] << 1;
+        x[i] = t % 10;
     }
-    return r % MOD;
-}
-
-int fact(const long long &n) {
-    int P = 1;
-
-    for (int i = 2; i <= n; ++i) {
-        P = P * i % MOD;
+    for (; t; t /= 10) {
+        x[++x[0]] = t % 10;
     }
-
-    return P;
 }
 
-long long square(const long long n) {
-    return n * n % MOD;
-}
-
-long long perms(const int n, const int zeros, const int ones) {
-    const long long top = fact(n);
-    const long long bottom = (fact(zeros) * square(fact(ones))) % MOD;
-    long long invMod = modPow(bottom, MOD - 2);
-    return top * invMod % MOD;
-}
-
-long long solve(const int &n) {
-    long long S = 0;
-
-    const long long halfN = n / 2;
-
-    for (int i = 0; i <= halfN; ++i) {
-        S = (S + perms(n, n - 2 * i, i)) % MOD;
+void write(big x) {
+    for (int i = x[0]; i; --i) {
+        cout << x[i];
     }
-
-    return S;
 }
 
 int main() {
     int n;
-
     cin >> n;
-    cout << solve(n);
 
+    big res;
+
+    res[0] = 1;
+    res[1] = 3;
+
+    for (int i = 1; i < n; ++i) {
+        bigDouble(res);
+    }
+
+
+    write(res);
     return 0;
 }
